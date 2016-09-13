2ecb309d36830ad9fd43cf607e0a294e4766587f
Krishna Mohan <mohankri@gmail.com>
2016-09-12 18:20:59 -0700

    Hello World

---
+++ hello.asm
@@ -0,0 +1,38 @@
+section .data
+	msg db	"hello, world"
+
+section .text
+	global _start
+
+
+_start:
+     
+    ; 
+	; sys_write(fd, buf, count);
+    ; rax: syscall number 1 = syscall number i.e sys_write
+    ;			rdi = 1 i.e stdout
+	;			rsi = buffer i.e msg
+    ;			rdx = count i.e string length;
+    ;
+
+	; put 1 to rax register rax-temp register; when we call syscall rax mush contain syscall number
+	mov rax, 1	
+
+	; put 1 to rdi register: rdi: used to pass 1st argument to the function.
+	mov rdi, 1 
+
+	; rsi: used to pass 2nd argument to the function.
+	mov rsi, msg 
+
+	; rdx: used to pass 3rd argument to the function.
+	mov rdx, 13  
+
+	syscall
+
+	; 60 is syscall number of exit	
+	mov rax, 60 
+
+	; error code of the program
+	mov rdi, 0 
+	
+	syscall
